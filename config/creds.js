7c6df38d5a8eb14f580e25f7395b045ec3fc8a43
Rick Thomas at sevaa <rick@evenview.com>
2012-02-06 23:51:10 +0300

    remove creds

--- config/creds.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    MEETUP_CLIENT_ID: "vkb1o5krta6715kvo5kvikf9kk",
-    MEETUP_CLIENT_SECRET: "jr3taf32fadgloi7og5fcl6sdr",
-    GITHUB_CLIENT_ID: "0f7c0a060a2f30cefba6",
-    GITHUB_CLIENT_SECRET: "205d5fad5b13eca29f8352daae7bace96a829016",
-    SESSION_SECRET: "5p4k3m",
-    ADMIN_TOKEN: "e058981f982b7988fb821f7bafe1cf8bc364cb47",
-    TEAM_ID: "125187"
-}
